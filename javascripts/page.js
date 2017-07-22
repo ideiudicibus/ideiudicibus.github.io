7942b22f859df63e3cbc15c61020d5c61e4d898a
ignazio de iudicibus <i.deiudicibus@gmail.com>
2017-07-22 17:30:23 +0200

    update pages

--- javascripts/page.js
+++
@@ -1,9 +0,0 @@
-$('#download-cv').click(function(e) {
-    e.preventDefault();  //stop the browser from following
-    window.location.href = 'doc/IGNAZIO_DE_IUDICIBUS_EN.pdf';
-});
-
-$('#email-me').click(function(e) {
-    e.preventDefault();  //stop the browser from following
-    window.location.href = "mailto:i.deiudicibus@gmail.com";
-});
